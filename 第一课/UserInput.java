12b67a37e074410d011b2b8d36b1750e261c66d5
wq <2656866321@qq.com>
2023-03-01 14:11:19 +0800

    test git commit

--- 第一课/UserInput.java
+++ 第一课/UserInput.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 
 public class UserInput {
     public static void main(String[] args){
+        //
         Scanner scanner = new Scanner(System.in);
         System.out.println("请输入一个整数的年龄：");
         int age = scanner.nextInt();
