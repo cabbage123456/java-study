e8ad8ed8b1672f94d47e533caca65faaa4e7beb6
wq <2656866321@qq.com>
2023-03-07 21:14:23 +0800

    java study 1

---
+++ 第二课/ValueDemo1.java
@@ -0,0 +1,18 @@
+public class ValueDemo1 {
+    public static void main(String[] args){
+        //整数
+        System.out.println(666);
+        System.out.println(-777);
+        //小数
+        System.out.println(3.14);
+        //字符串
+        System.out.println("123");
+        //字符
+        System.out.println('w');
+        //布尔
+        System.out.println(true);
+        System.out.println(false);
+        //空 null只能用字符串的形式进行打印
+        System.out.println("null");
+    }
+}
