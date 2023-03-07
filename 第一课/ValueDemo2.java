5a72a1169ba9b7e1e31ca6b269813fdbbeabd3a9
wq <2656866321@qq.com>
2023-03-07 19:48:46 +0800

    第一课  课后作业

---
+++ 第一课/ValueDemo2.java
@@ -0,0 +1,7 @@
+public class ValueDemo2 {
+    //熟悉制表符的基本用法
+    public static void main(String[] args){
+        System.out.println("name"+"age");
+        System.out.println("name"+'\t'+"age");
+    }
+}
