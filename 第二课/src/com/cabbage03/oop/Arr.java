c41348ad274edd4eccbaad152134489e2eb2b68c
wq <2656866321@qq.com>
2023-03-20 12:32:49 +0800

    数组学习1

---
+++ 第二课/src/com/cabbage03/oop/Arr.java
@@ -0,0 +1,12 @@
+package com.cabbage03.oop;
+//数组的遍历操作
+//数组名.length
+public class Arr {
+    public static void main(String[] args){
+        int[] arr={3,5,7,8,4};
+        for(int i=0;i<arr.length;i++) {
+            System.out.println(arr[i]);
+
+        }
+    }
+}
